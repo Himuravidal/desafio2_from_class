c5bcaa6ecef1c068eb971bbe448efa155044b7b0
Cris <cristian.vidal.lopezgmail.com>
2020-08-20 17:02:54 -0400

    This was the advance of the day.

---
+++ app/src/main/java/cl/desafiolatam/desafiodos/model/db/TaskDao.kt
@@ -0,0 +1,22 @@
+package cl.desafiolatam.desafiodos.model.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import cl.desafiolatam.desafiodos.model.Task
+
+@Dao
+interface TaskDao {
+
+    @Insert
+    suspend fun insertTaskOnDatabase(task: Task)
+
+    @Update
+    suspend fun updateTaskOnDatabase(task: Task)
+
+    @Delete
+    suspend fun deleteTaskOnDatabase(task: Task)
+
+    @Query("SELECT * FROM task_table")
+    fun getAllTaskOnDatabase(): LiveData<List<Task>>
+
+}
